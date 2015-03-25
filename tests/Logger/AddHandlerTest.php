07e63e3b8a78d61b349a165e9855c3fec560d003
H Hatfield <hhatfield@deseretdigital.com>
2015-03-25 12:40:11 -0600

    removed singleton and added more test

---
+++ tests/Logger/AddHandlerTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hhatfield
+ * Date: 3/23/15
+ * Time: 10:22 PM
+ */
+
+namespace DDM\Logger\Tests;
+
+use DDM\Logger\Logger;
+use Psr\Log\InvalidArgumentException;
+use Psr\Log\NullLogger;
+use Psr\Log\LogLevel;
+
+class AddHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    public function setUp()
+    {
+        $this->logger = new Logger();
+    }
+
+    /**
+     * @test
+     */
+    public function canAddToValidLevel()
+    {
+
+        $this->logger->addHandler(new NullLogger(), LogLevel::INFO);
+
+        $this->assertEquals(
+            array(new NullLogger()),
+            $this->logger->getHandlers(LogLevel::INFO),
+            'Should have received Null Logger'
+        );
+
+    }
+
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function cannotAddToInvalidLevel()
+    {
+        $this->logger->addHandler(new NullLogger(), 'invalid');
+    }
+
+    /** @test */
+    public function canAddToMultipleLevels()
+    {
+        $levels = array(
+            LogLevel::ALERT,
+            LogLevel::INFO
+        );
+
+        $logger = new NullLogger();
+        $this->logger->addHandler($logger, $levels);
+        $this->assertSame(
+            array($logger),
+            $this->logger->getHandlers(LogLevel::ALERT),
+            'Should have been added to Alert level'
+        );
+        $this->assertSame(
+            array($logger),
+            $this->logger->getHandlers(LogLevel::INFO),
+            'Should have been added to Info level'
+        );
+    }
+
+    /** @test */
+    public function canAddsMultipleHandlersToOneLevel()
+    {
+        //arrange
+        $testHandler = new NullLogger();
+
+        $expected = array(
+            LogLevel::EMERGENCY=>array(),
+            LogLevel::ALERT=>array(),
+            LogLevel::CRITICAL=>array(),
+            LogLevel::ERROR=>array(),
+            LogLevel::WARNING=>array(),
+            LogLevel::NOTICE=>array(),
+            LogLevel::INFO=>array(
+                $testHandler,
+                $testHandler
+            ),
+            LogLevel::DEBUG=>array(),
+        );
+
+        //act
+        $this->logger->addHandler($testHandler, LogLevel::INFO);
+        $this->logger->addHandler($testHandler, LogLevel::INFO);
+
+        //assert
+        $this->assertEquals(
+            $expected,
+            $this->logger->getHandlers(),
+            'Should have received Null Logger'
+        );
+    }
+}
---
+++ tests/Logger/LogTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hhatfield
+ * Date: 3/14/15
+ * Time: 2:54 PM
+ */
+
+namespace DDM\Logger\Tests;
+
+use DDM\Logger\Logger;
+use Psr\Log\LogLevel;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Mockery;
+
+class LogTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    public function setUp()
+    {
+        $this->logger = new Logger();
+    }
+
+
+    /** @test */
+    public function itCallsOneHandler()
+    {
+        //arrange
+        $nullMock = Mockery::mock('\Psr\Log\LoggerInterface');
+        $this->logger->addHandler($nullMock, LogLevel::INFO);
+        $nullMock->shouldReceive('log')->once();
+        //act
+        $this->logger->log(LogLevel::INFO, 'happiness');
+        //assert
+    }
+
+    /** @test */
+    public function itCallsOneHandlerMultipleLevels()
+    {
+        //arrange
+        $nullMock = Mockery::mock('\Psr\Log\LoggerInterface');
+        $this->logger->addHandler($nullMock, LogLevel::INFO);
+        $this->logger->addHandler($nullMock, LogLevel::WARNING);
+        $nullMock->shouldReceive('log')->once();
+        //act
+        $this->logger->log(LogLevel::INFO, 'happiness');
+        $this->logger->log(LogLevel::WARNING, 'happiness');
+        //assert
+    }
+
+}
